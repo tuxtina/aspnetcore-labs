df33628ee3eabbc7a6c761f22c9324996dda2361
tuxtina <tuxtina@users.noreply.github.com>
2016-06-22 12:15:40 +0200

    Added config.

--- Lab2/Startup.cs
+++ Lab2/Startup.cs
@@ -6,11 +6,21 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace Lab2
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env) {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", reloadOnChange: true, optional: true)
+                .Build();
+        }
+    
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,15 +31,15 @@ namespace Lab2
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-        {
+            {
                 app.UseDeveloperExceptionPage();
             }
             // set environment using ASPNETCORE_ENVIRONMENT env var
             app.UseStaticFiles();
-
+ 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"Hello {env.EnvironmentName}!");
+                await context.Response.WriteAsync($"{Configuration["message"]}");
             });
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DWX2016 ASP.NET Core Workshop 
 https://github.com/shanselman/aspnetcore-workshop
+
+Notes
+* http://sourceof.net goes straight to the source code for .NET
+* https://live.asp.net source code at https://github.com/aspnet/live.asp.net
+
