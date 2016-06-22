a72a53841bf0e526611182b80066d24eb2492b72
tuxtina <tuxtina@users.noreply.github.com>
2016-06-22 12:19:49 +0200

    Added routing.

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ https://github.com/shanselman/aspnetcore-workshop
 Notes
 * http://sourceof.net goes straight to the source code for .NET
 * https://live.asp.net source code at https://github.com/aspnet/live.asp.net
+* Updating with SignalR http://www.hanselman.com/blog/PennyPinchingInTheCloudHowToRunATwoDayVirtualConferenceFor10.aspx
 
---
+++ lab3/Startup.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Routing;
+
+namespace Lab2
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env) {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", reloadOnChange: true, optional: true)
+                .Build();
+        }
+    
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("", context => context.Response.WriteAsync("Hello from Routing!"));
+            app.UseRouter(routeBuilder.Build());
+        }
+    }
+}
