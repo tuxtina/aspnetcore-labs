fc70c77805b737bfe12b60f9c6a2c8e294bb57d1
tuxtina <tuxtina@users.noreply.github.com>
2016-06-22 10:59:53 +0200

    Added static files.

--- Lab2/Startup.cs
+++ Lab2/Startup.cs
@@ -20,6 +20,8 @@ namespace Lab2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
